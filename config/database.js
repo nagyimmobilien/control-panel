cd2d50c517f869d4e248cf05697785fb840d74af
b.kovacs <b.kovacs@nagyimmobilien.at>
2025-09-25 12:18:50 +0200

    Initialize MongoDB connection

---
+++ config/database.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined.");
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const clientPromise = client.connect().then(() => {
+  console.log("MongoDB connected successfully!");
+  return client;
+}).catch(err => {
+  console.error("Failed to connect to MongoDB!", err);
+  throw err;
+});
+
+export default clientPromise;
